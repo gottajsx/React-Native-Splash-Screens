e22ecd7192de3a53a253d10ac94e632a7abd32ea
gottajsx <gottajsx@gmail.com>
2025-03-13 15:50:17 +0100

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { Link, Stack } from 'expo-router';
+
+const DayDetailsScreen = () => {
+  return (
+    <View>
+      <Stack.Screen options={{ title: 'Onboarding Example' }} />
+
+      <Link href="onboarding" asChild>
+        <Button title="Go to onboarding" />
+      </Link>
+    </View>
+  );
+};
+
+export default DayDetailsScreen;
